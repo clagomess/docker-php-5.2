477f4987d27512c8f1a609879b1c0c798c119307
Cláudio Gomes <cla.gomess@gmail.com>
2025-05-25 23:39:12 -0300

    removed xdebug.log from init; update docker-compose

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ echo "" > /var/log/php/xdebug.log
 echo "" > /var/log/apache/access_log
 echo "" > /var/log/apache/error_log
 
-tail -f /var/log/php/xdebug.log /var/log/apache/access_log > /dev/stdout & \
+tail -f /var/log/apache/access_log > /dev/stdout & \
 tail -f /var/log/php/error.log /var/log/apache/error_log > /dev/stderr & \
 /opt/httpd-2.2.3/bin/apachectl -D FOREGROUND
